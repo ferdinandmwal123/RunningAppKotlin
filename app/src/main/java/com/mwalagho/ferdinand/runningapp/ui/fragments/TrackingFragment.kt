249fbb544469d3403318a34d4480e9b5e5b30b64
ferdinand <ferdinandmwal@gmail.com>
2021-01-25 12:30:32 +0300

    Cancel Dialog

--- app/src/main/java/com/mwalagho/ferdinand/runningapp/ui/fragments/TrackingFragment.kt
+++ app/src/main/java/com/mwalagho/ferdinand/runningapp/ui/fragments/TrackingFragment.kt
@@ -2,16 +2,19 @@ package com.mwalagho.ferdinand.runningapp.ui.fragments
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
+import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.model.PolylineOptions
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.mwalagho.ferdinand.runningapp.R
 import com.mwalagho.ferdinand.runningapp.other.Constants.ACTION_PAUSE_SERVICE
 import com.mwalagho.ferdinand.runningapp.other.Constants.ACTION_START_OR_RESUME_SERVICE
+import com.mwalagho.ferdinand.runningapp.other.Constants.ACTION_STOP_SERVICE
 import com.mwalagho.ferdinand.runningapp.other.Constants.MAP_ZOOM
 import com.mwalagho.ferdinand.runningapp.other.Constants.POLYLINE_COLOR
 import com.mwalagho.ferdinand.runningapp.other.Constants.POLYLINE_WIDTH
@@ -33,6 +36,17 @@ class TrackingFragment : Fragment(R.layout.fragment_tracking) {
     private var map: GoogleMap? = null
 
     private var currentTimeInMillis = 0L
+
+    private var menu: Menu? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        setHasOptionsMenu(true)
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -71,6 +85,49 @@ class TrackingFragment : Fragment(R.layout.fragment_tracking) {
         } else {
             sendCommandToService(ACTION_START_OR_RESUME_SERVICE)
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.toolbar_tracking_menu, menu)
+        this.menu = menu
+    }
+
+    override fun onPrepareOptionsMenu(menu: Menu) {
+        super.onPrepareOptionsMenu(menu)
+        if (currentTimeInMillis > 0L){
+            this.menu?.getItem(0)?.isVisible = true
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.miCancelTracking -> {
+                showCancelTrackingDialog()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun showCancelTrackingDialog(){
+        val dialog = MaterialAlertDialogBuilder(requireContext(),R.style.ThemeOverlay_AppCompat_Dialog_Alert)
+            .setTitle("Cancel the run?")
+            .setMessage("Are you sure?")
+            .setIcon(R.drawable.ic_delete)
+            .setPositiveButton("Yes"){ _,_ ->
+                stopRun()
+            }
+            .setNegativeButton("No"){dialog, _ ->
+                dialog.cancel()
+
+            }
+            .create()
+        dialog.show()
+    }
+
+    private fun stopRun(){
+        sendCommandToService(ACTION_STOP_SERVICE)
+        findNavController().navigate(R.id.action_trackingFragment_to_runFragment)
     }
 
     private fun updateTracking(isTracking : Boolean){
