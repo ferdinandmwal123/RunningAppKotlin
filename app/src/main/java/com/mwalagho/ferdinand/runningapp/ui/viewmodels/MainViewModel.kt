ce011f02ea6beb69ec8f37c7af7f85375d89fa8c
ferdinand <ferdinandmwal@gmail.com>
2021-02-12 14:06:16 +0300

    Medial Live Data

--- app/src/main/java/com/mwalagho/ferdinand/runningapp/ui/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/mwalagho/ferdinand/runningapp/ui/viewmodels/MainViewModel.kt
@@ -1,9 +1,11 @@
 package com.mwalagho.ferdinand.runningapp.ui.viewmodels
 
 import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.mwalagho.ferdinand.runningapp.db.Run
+import com.mwalagho.ferdinand.runningapp.other.SortType
 import com.mwalagho.ferdinand.runningapp.repositories.MainRepository
 import kotlinx.coroutines.launch
 
@@ -11,11 +13,22 @@ class MainViewModel @ViewModelInject constructor(
     val mainRepository: MainRepository
 ) : ViewModel() {
 
-    val runsSortedByDate = mainRepository.getAllRunsSortedByDate()
-    val runsSortedByDistance = mainRepository.getAllRunsSortedByDistance()
-    val runsSortedByCaloriesBurned = mainRepository.getAllRunsSortedByCaloriesBurned()
-    val runsSortedByTimeInMillis = mainRepository.getAllRunsSortedByTimeInMillis()
-    val runsSortedByAvgSpeed= mainRepository.getAllRunsSortedByAvgSpeed()
+    private val runsSortedByDate = mainRepository.getAllRunsSortedByDate()
+    private val runsSortedByDistance = mainRepository.getAllRunsSortedByDistance()
+    private val runsSortedByCaloriesBurned = mainRepository.getAllRunsSortedByCaloriesBurned()
+    private val runsSortedByTimeInMillis = mainRepository.getAllRunsSortedByTimeInMillis()
+    private val runsSortedByAvgSpeed = mainRepository.getAllRunsSortedByAvgSpeed()
+
+    val runs = MediatorLiveData<List<Run>>()
+    val sortType = SortType.DATE
+
+    init {
+        runs.addSource(runsSortedByDate) { result ->
+            if(sortType == SortType.DATE){
+                result.let { runs.value = it }
+            }
+        }
+    }
 
     fun insertRun(run: Run) = viewModelScope.launch {
         mainRepository.insertRun(run)
