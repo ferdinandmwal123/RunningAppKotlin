6eae6c0852a20114f584c40cdcd4c5f3a3cb8e3e
ferdinand <ferdinandmwal@gmail.com>
2021-03-15 13:17:28 +0300

    SharedPrefernces

--- app/src/main/java/com/mwalagho/ferdinand/runningapp/ui/fragments/SetupFragment.kt
+++ app/src/main/java/com/mwalagho/ferdinand/runningapp/ui/fragments/SetupFragment.kt
@@ -1,13 +1,24 @@
 package com.mwalagho.ferdinand.runningapp.ui.fragments
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.mwalagho.ferdinand.runningapp.R
+import com.mwalagho.ferdinand.runningapp.other.Constants.KEY_FIRST_TIME_TOGGLE
+import com.mwalagho.ferdinand.runningapp.other.Constants.KEY_NAME
+import com.mwalagho.ferdinand.runningapp.other.Constants.KEY_WEIGHT
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_setup.*
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class SetupFragment: Fragment(R.layout.fragment_setup) {
+
+    @Inject
+    lateinit var sharedPref : SharedPreferences
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -18,6 +29,19 @@ class SetupFragment: Fragment(R.layout.fragment_setup) {
     }
 
     private fun writePersonalDataToSharedPref(): Boolean{
+        val name = etName.text.toString()
+        val weight = etWeight.text.toString()
 
+        if(name.isEmpty() || weight.isEmpty()){
+            return false
+        }
+        sharedPref.edit()
+            .putString(KEY_NAME,name)
+            .putFloat(KEY_WEIGHT, weight.toFloat())
+            .putBoolean(KEY_FIRST_TIME_TOGGLE,false)
+            .apply()
+        val toolbarText = "Let's go $name!"
+        requireActivity().tvToolbarTitle.text = toolbarText
+        return true
     }
 }
