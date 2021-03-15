662825e53937ac8b1193a68ac124c242584ed452
ferdinand <ferdinandmwal@gmail.com>
2021-03-15 13:26:30 +0300

    some stuff

--- app/src/main/java/com/mwalagho/ferdinand/runningapp/ui/fragments/SetupFragment.kt
+++ app/src/main/java/com/mwalagho/ferdinand/runningapp/ui/fragments/SetupFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.snackbar.Snackbar
 import com.mwalagho.ferdinand.runningapp.R
 import com.mwalagho.ferdinand.runningapp.other.Constants.KEY_FIRST_TIME_TOGGLE
 import com.mwalagho.ferdinand.runningapp.other.Constants.KEY_NAME
@@ -15,30 +16,40 @@ import kotlinx.android.synthetic.main.fragment_setup.*
 import javax.inject.Inject
 
 @AndroidEntryPoint
-class SetupFragment: Fragment(R.layout.fragment_setup) {
+class SetupFragment : Fragment(R.layout.fragment_setup) {
 
     @Inject
-    lateinit var sharedPref : SharedPreferences
+    lateinit var sharedPref: SharedPreferences
+
+    @Inject
+    var isFirstAppOpen = true
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        tvContinue.setOnClickListener{
-            findNavController().navigate(R.id.action_setupFragment_to_runFragment)
+        tvContinue.setOnClickListener {
+            val success = writePersonalDataToSharedPref()
+            if (success) {
+                findNavController().navigate(R.id.action_setupFragment_to_runFragment)
+
+            } else {
+                Snackbar.make(requireView(), "Please enter all fields", Snackbar.LENGTH_SHORT)
+                    .show()
+            }
         }
     }
 
-    private fun writePersonalDataToSharedPref(): Boolean{
+    private fun writePersonalDataToSharedPref(): Boolean {
         val name = etName.text.toString()
         val weight = etWeight.text.toString()
 
-        if(name.isEmpty() || weight.isEmpty()){
+        if (name.isEmpty() || weight.isEmpty()) {
             return false
         }
         sharedPref.edit()
-            .putString(KEY_NAME,name)
+            .putString(KEY_NAME, name)
             .putFloat(KEY_WEIGHT, weight.toFloat())
-            .putBoolean(KEY_FIRST_TIME_TOGGLE,false)
+            .putBoolean(KEY_FIRST_TIME_TOGGLE, false)
             .apply()
         val toolbarText = "Let's go $name!"
         requireActivity().tvToolbarTitle.text = toolbarText
