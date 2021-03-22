a715fa19b5268c7b44c7bc5a4ddd5c1a889f37ff
ferdinand <ferdinandmwal@gmail.com>
2021-03-22 15:44:47 +0300

    functions

--- app/src/main/java/com/mwalagho/ferdinand/runningapp/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/com/mwalagho/ferdinand/runningapp/ui/fragments/SettingsFragment.kt
@@ -1,7 +1,38 @@
 package com.mwalagho.ferdinand.runningapp.ui.fragments
 
+import android.content.SharedPreferences
 import androidx.fragment.app.Fragment
 import com.mwalagho.ferdinand.runningapp.R
+import com.mwalagho.ferdinand.runningapp.other.Constants.KEY_NAME
+import com.mwalagho.ferdinand.runningapp.other.Constants.KEY_WEIGHT
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_settings.*
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class SettingsFragment: Fragment(R.layout.fragment_settings) {
+
+    @Inject
+    lateinit var sharedPreferences: SharedPreferences
+
+    private fun loadFieldsFromSharedPref(){
+        val name = sharedPreferences.getString(KEY_NAME,"")
+        val weight = sharedPreferences.getFloat(KEY_WEIGHT,80f)
+    }
+    private fun applyChangesToSharedPref(): Boolean{
+        val nameText = etName.text.toString()
+        val weightText  = etWeight.text.toString()
+        if(nameText.isEmpty() || weightText.isEmpty()){
+            return false
+        }
+        sharedPreferences.edit()
+            .putString(KEY_NAME, nameText)
+            .putFloat(KEY_WEIGHT, weightText.toFloat())
+            .apply()
+        val toolbarText = "Let's go $nameText"
+        requireActivity().tvToolbarTitle.setText(toolbarText)
+        return true
+    }
+
 }
