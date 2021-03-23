85a60c5dfe7bdd3fd68ff20fd0d6f17b6a3852d5
ferdinand <ferdinandmwal@gmail.com>
2021-03-23 15:17:19 +0300

    mview

--- app/src/main/java/com/mwalagho/ferdinand/runningapp/other/CustomMarkerView.kt
+++ app/src/main/java/com/mwalagho/ferdinand/runningapp/other/CustomMarkerView.kt
@@ -1,4 +1,19 @@
 package com.mwalagho.ferdinand.runningapp.other
 
-class CustomMarkerView {
+import android.content.Context
+import com.github.mikephil.charting.components.MarkerView
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.highlight.Highlight
+import com.mwalagho.ferdinand.runningapp.db.Run
+
+class CustomMarkerView(
+    val runs: List<Run>,
+    c: Context,
+    layoutId: Int
+) : MarkerView(c, layoutId) {
+
+
+    override fun refreshContent(e: Entry?, highlight: Highlight?) {
+        super.refreshContent(e, highlight)
+    }
 }
